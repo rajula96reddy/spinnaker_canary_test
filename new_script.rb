05d3c3f99db169da75b5c328120ab21ea6f3859d
Rajula Vineet Reddy <rajula96reddy@gmail.com>
2019-09-26 17:03:31 +0530

    Update new_script.rb

--- new_script.rb
+++ new_script.rb
@@ -1,7 +1,7 @@
 counts = Hash.new(0)
 
 ARGV[1].to_i.times do
-  output = `curl --insecure -H "Host: echo.com" -s https://#{ARGV[0]} | grep 'hostname'`
+  output = `curl --insecure -H "Host: #{ARGV[2]}" -s https://#{ARGV[0]} | grep 'hostname'`
   counts[output.strip.split.last] += 1
 end
 
